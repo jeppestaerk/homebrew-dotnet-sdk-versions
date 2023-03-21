797fd940d988389004914c3c39c133458de00bcc
Jeppe Stærk <jeppe.staerk@schibsted.com>
2023-03-21 14:56:08 +0100

    sdk8 preview added

--- Casks/dotnet-sdk8-preview.rb
+++ Casks/dotnet-sdk8-preview.rb
@@ -3,17 +3,17 @@ cask "dotnet-sdk8-preview" do
 
   version "8.0.100-preview.2.23157.25"
 
-  sha256_x64 = "246cc88f89638475d56384855825381990650464266ca8b0de17fdca5c90d16c8d2ce27aa27bd01250d7b9a12f7fa67bef5e49277fe4bcaecad831e3f8eddafd"
-  sha256_arm64 = "e0802a394d043a56ec95dab56520aba933f271fc4d8098833b9f36e1943dc9897bb727bac503c0c6e46fa9b5db446c804be27209421896461acc6c848d9d6c3a"
+  sha512_x64 = "246cc88f89638475d56384855825381990650464266ca8b0de17fdca5c90d16c8d2ce27aa27bd01250d7b9a12f7fa67bef5e49277fe4bcaecad831e3f8eddafd"
+  sha512_arm64 = "e0802a394d043a56ec95dab56520aba933f271fc4d8098833b9f36e1943dc9897bb727bac503c0c6e46fa9b5db446c804be27209421896461acc6c848d9d6c3a"
   url_x64 = "https://download.visualstudio.microsoft.com/download/pr/d22c5e44-5ddc-41c2-bc23-cc7cbf7bed72/25e24c6de0c41648965533073cfba2c2/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
   url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/9bb7054e-4547-4021-b46f-edee2428b10d/1dbe4945aabec0cd9c8b15080ec98b37/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_intel do
-    sha256 sha256_x64
+    sha512 sha512_x64
     url url_x64
   end
   on_arm do
-    sha256 sha256_arm64
+    sha512 sha512_arm64
     url url_arm64
   end
 
